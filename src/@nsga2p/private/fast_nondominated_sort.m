5bdaaac32cd35ce2829af330967e028e2c97b7dc
simondlevy <simon.d.levy@gmail.com>
2020-06-23 20:00:01 -0400

    Cleanup

--- src/@nsga2p/private/fast_nondominated_sort.m
+++ src/@nsga2p/private/fast_nondominated_sort.m
@@ -1,7 +1,7 @@
 function F = fast_nondominated_sort(P)
 % From same-named algorithm of Deb et al. (2002).
 % Returns a set of non-dominated fronts F of population P,
-% sorted by non-domination ranks. 
+% sorted by non-domination ranks.
 %
 % Copyright (c) 2019 Simon D. Levy
 %
@@ -10,15 +10,43 @@ function F = fast_nondominated_sort(P)
 % i is the front-counter and is initialized to one
 i = 1;
 
+F = {};
+
 while ~isempty(P)
     
-    % find the non-dominated front
-    F{i} = find_nondominated_front(P);
+    % start with nobody on front
+    Fi = {};
     
+    % look at each fitness in the population
+    for i = 1:length(P)
+        
+        % assume undominateded
+        dominated = false;
+        
+        % compare it with everyone (including itself, costs less time)
+        for j = 1:length(P)
+            
+            % if it's dominated by anyone, mark it as such and stop looking
+            if dominates(P{j}, P{i})
+                dominated = true;
+                break
+            end
+            
+        end
+        
+        % add to front if not dominated
+        if ~dominated
+            Fi{end+1} = P{i};
+        end
+        
+    end
+
     % remove the non-dominated solutions from P
-    P = remove(P, F{i});
+    P = remove(P, Fi);
     
     % increment the front-counter
     i = i + 1;
+    
+    F{end+1} = Fi;
 end
 
