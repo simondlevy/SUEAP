83f982bf7744d741ff4cc6a0e42d2012133a15dc
simondlevy <simon.d.levy@gmail.com>
2020-07-19 18:28:40 -0400

    More Elitist

--- python/sueap/elitist/__init__.py
+++ python/sueap/elitist/__init__.py
@@ -6,3 +6,12 @@ Copyright (C) 2020 Simon D. Levy
 MIT License
 '''
 
+import numpy as np
+
+class Elitist:
+
+    def __init__(self, noise_std=0.01, pop_size=2000, parents_count=10, max_gen=None, max_evals=10, max_reward=None):
+
+        return
+
+
