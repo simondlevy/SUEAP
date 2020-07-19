7e4b5e466783581ce3c5e4ac64342828300f34b2
simondlevy <simon.d.levy@gmail.com>
2020-07-19 18:45:26 -0400

    More Elitist

--- python/sueap/elitist/__init__.py
+++ python/sueap/elitist/__init__.py
@@ -10,8 +10,13 @@ import numpy as np
 
 class Elitist:
 
-    def __init__(self, noise_std=0.01, pop_size=2000, parents_count=10, max_gen=None, max_evals=10, max_reward=None):
+    def __init__(self, noise_std=0.01, pop_size=2000, parents_count=10, max_gen=None, max_fitness=None):
 
-        return
+        self.noise_std = noise_std
+        self.pop_size = pop_size
+        self.parents_count = parents_count
+        self.max_gen = np.iinfo(np.uint32).max if max_gen is None else max_gen
+        self.max_fitness = np.inf if max_fitness is None else max_fitness
+
 
 
