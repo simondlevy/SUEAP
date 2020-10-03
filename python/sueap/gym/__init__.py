5c041684865f4c4d914e37fce6d2445d99f29f34
simondlevy <simon.d.levy@gmail.com>
2020-10-03 01:15:07 -0400

    More multi-objective gym work

--- python/sueap/gym/__init__.py
+++ python/sueap/gym/__init__.py
@@ -25,6 +25,57 @@ class _Problem:
         self.seed = seed
         self.env_name = env_name
 
+    def eval_params(self, params, episodes):
+
+        total_reward = self.initial_reward()
+        total_steps = 0
+
+        for _ in range(episodes):
+
+            episode_reward, episode_steps = self.run_episode(params)
+
+            total_reward += episode_reward
+            total_steps += episode_steps
+
+        return total_reward, total_steps
+
+    def run_episode(self, params, render=False):
+
+        episode_reward = self.initial_reward
+        episode_steps = 0
+
+        # Build env
+        env = gym.make(self.env_name)
+
+        if self.seed is not None:
+            env.seed(self.seed)
+
+        obs = env.reset()
+
+        # Simulation loop
+        while True:
+
+            action = self.get_action(params, obs)
+
+            # Optional render of environment
+            if render:
+                env.render()
+
+            # Do environment step
+            obs, reward, done, _ = self.env(env, action)
+
+            episode_reward += reward
+            episode_steps += 1
+
+            # Episode end
+            if done:
+                break
+
+        # Cleanup
+        env.close()
+
+        return episode_reward, episode_steps
+
 class MultiobjectiveProblem(_Problem):
 
     def __init__(self, env_name, ndims, seed=None):
@@ -33,62 +84,13 @@ class MultiobjectiveProblem(_Problem):
 
         self.ndims = ndims
 
-    def eval_params(self, params, episodes=10):
-        '''
-        Evaluates parameters for a given number of episodes
-        Inputs:
-            params   paramters to evaluate
-            episodes number of episodes to run
-        Returns: total rewards and total number of steps taken
-        '''
+    def initial_reward(self):
 
-        total_reward = np.zeros(self.ndims)
-        total_steps = 0
+        return np.zeros(self.ndims)
 
-        for _ in range(episodes):
+    def step(self, env, action):
 
-            episode_reward, episode_steps = self.run_episode(params)
-
-            total_reward += episode_reward
-            total_steps += episode_steps
-
-        return total_reward, total_steps
-
-    def run_episode(self, params, render=False):
-
-        # Build env
-        env = gym.make(self.env_name)
-
-        if self.seed is not None:
-            env.seed(self.seed)
-
-        obs = env.reset()
-
-        episode_reward, episode_steps = self.ndims,0
-
-        # Simulation loop
-        while True:
-
-            action = self.get_action(params, obs)
-
-            # Optional render of environment
-            if render:
-                env.render()
-
-            # Do environment step
-            obs, reward, done, _ = env.step_mo(action)
-
-            episode_reward += reward
-            episode_steps += 1
-
-            # Episode end
-            if done:
-                break
-
-        # Cleanup
-        env.close()
-
-        return episode_reward, episode_steps
+        return env.step_mo(action)
 
 class Problem(_Problem):
 
@@ -96,61 +98,12 @@ class Problem(_Problem):
 
         _Problem.__init__(self, env_name, seed)
 
-    def eval_params(self, params, episodes=10):
-        '''
-        Evaluates parameters for a given number of episodes
-        Inputs:
-            params   paramters to evaluate
-            episodes number of episodes to run
-        Returns: total rewards and total number of steps taken
-        '''
+    def initial_reward(self):
 
-        total_reward = 0
-        total_steps = 0
-
-        for _ in range(episodes):
-
-            episode_reward, episode_steps = self.run_episode(params)
-
-            total_reward += episode_reward
-            total_steps += episode_steps
-
-        return total_reward, total_steps
+        return 0
     
-    def run_episode(self, params, render=False):
+    def step(self, env, action):
 
-        # Build env
-        env = gym.make(self.env_name)
-
-        if self.seed is not None:
-            env.seed(self.seed)
-
-        obs = env.reset()
-
-        episode_reward, episode_steps = 0,0
-
-        # Simulation loop
-        while True:
-
-            action = self.get_action(params, obs)
-
-            # Optional render of environment
-            if render:
-                env.render()
-
-            # Do environment step
-            obs, reward, done, _ = env.step(action)
-
-            episode_reward += reward
-            episode_steps += 1
-
-            # Episode end
-            if done:
-                break
-
-        # Cleanup
-        env.close()
-
-        return episode_reward, episode_steps
+        return env.step(action)
 
 
