7934cf36c6601220dfe1abb76586140a5385eaf7
simondlevy <simon.d.levy@gmail.com>
2020-10-03 00:18:46 -0400

    Fixed multi-objective gym

--- python/sueap/gym/__init__.py
+++ python/sueap/gym/__init__.py
@@ -25,14 +25,99 @@ class _Problem:
         self.seed = seed
         self.env_name = env_name
 
-    def run_episode(self, params, render=False):
+class MultiobjectiveProblem(_Problem):
+
+    def __init__(self, env_name, ndims, seed=None):
+
+        _Problem.__init__(self, env_name, seed)
+
+        self.ndims = ndims
+
+    def eval_params(self, params, episodes=10):
         '''
-        Runs a single episode for training or testing
+        Evaluates parameters for a given number of episodes
         Inputs:
-            params paramters to evaluate
-            render set to True for real-time rendering
-        Returns: reward and number of steps taken
+            params   paramters to evaluate
+            episodes number of episodes to run
+        Returns: total rewards and total number of steps taken
         '''
+
+        total_reward = np.zeros(self.ndims)
+        total_steps = 0
+
+        for _ in range(episodes):
+
+            episode_reward, episode_steps = self.run_episode(params)
+
+            total_reward += episode_reward
+            total_steps += episode_steps
+
+        return total_reward, total_steps
+
+    def run_episode(self, params, render=False):
+
+        # Build env
+        env = gym.make(self.env_name)
+
+        if self.seed is not None:
+            env.seed(self.seed)
+
+        obs = env.reset()
+
+        episode_reward, episode_steps = self.ndims,0
+
+        # Simulation loop
+        while True:
+
+            action = self.get_action(params, obs)
+
+            # Optional render of environment
+            if render:
+                env.render()
+
+            # Do environment step
+            obs, reward, done, _ = env.step_mo(action)
+
+            episode_reward += reward
+            episode_steps += 1
+
+            # Episode end
+            if done:
+                break
+
+        # Cleanup
+        env.close()
+
+        return episode_reward, episode_steps
+
+class Problem(_Problem):
+
+    def __init__(self, env_name, seed=None):
+
+        _Problem.__init__(self, env_name, seed)
+
+    def eval_params(self, params, episodes=10):
+        '''
+        Evaluates parameters for a given number of episodes
+        Inputs:
+            params   paramters to evaluate
+            episodes number of episodes to run
+        Returns: total rewards and total number of steps taken
+        '''
+
+        total_reward = 0
+        total_steps = 0
+
+        for _ in range(episodes):
+
+            episode_reward, episode_steps = self.run_episode(params)
+
+            total_reward += episode_reward
+            total_steps += episode_steps
+
+        return total_reward, total_steps
+    
+    def run_episode(self, params, render=False):
 
         # Build env
         env = gym.make(self.env_name)
@@ -68,58 +153,4 @@ class _Problem:
 
         return episode_reward, episode_steps
 
-class MultiobjectiveProblem(_Problem):
 
-    def __init__(self, env_name, ndims, seed=None):
-
-        _Problem.__init__(self, env_name, seed)
-
-        self.ndims = ndims
-
-    def eval_params(self, params, episodes=10):
-        '''
-        Evaluates parameters for a given number of episodes
-        Inputs:
-            params   paramters to evaluate
-            episodes number of episodes to run
-        Returns: total rewards and total number of steps taken
-        '''
-
-        total_reward = np.zeros(self.ndims)
-        total_steps = 0
-
-        for _ in range(episodes):
-
-            episode_reward, episode_steps = self.run_episode(params)
-
-            total_reward += episode_reward
-            total_steps += episode_steps
-
-        return total_reward, total_steps
-
-class Problem(_Problem):
-
-    def __init__(self, env_name, seed=None):
-
-        _Problem.__init__(self, env_name, seed)
-
-    def eval_params(self, params, episodes=10):
-        '''
-        Evaluates parameters for a given number of episodes
-        Inputs:
-            params   paramters to evaluate
-            episodes number of episodes to run
-        Returns: total rewards and total number of steps taken
-        '''
-
-        total_reward = 0
-        total_steps = 0
-
-        for _ in range(episodes):
-
-            episode_reward, episode_steps = self.run_episode(params)
-
-            total_reward += episode_reward
-            total_steps += episode_steps
-
-        return total_reward, total_steps
